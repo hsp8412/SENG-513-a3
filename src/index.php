3ee31be851e8fa511dc41fa1d559c24b0ee60536
Sipeng He <sipeng.he@ucalgary.ca>
2024-10-22 00:22:46 -0600

    basic setup

--- src/index.php
+++ src/index.php
@@ -1,15 +1,29 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>My PHP Web Page</title>
+    <link rel="stylesheet" type="text/css" href="./styles.css">
 </head>
+
 <body>
-    <h1>Welcome to my PHP Web Page!</h1>
-    
-    <?php
-    // Your PHP code goes here
-    echo "Hello, world!";
-    ?>
-    
+    <div class="content">
+        <div class="card">
+            <h1>Welcome to my PHP Web Page!</h1>
+            <form action="">
+                <label for="email">Email:</label><br>
+                <input type="text" id="email" name="email"><br>
+                <label for="password">Password:</label><br>
+                <input type="password" id="password" name="password">
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    </div>
+
+
+
+
+
 </body>
+
 </html>
